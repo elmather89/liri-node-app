2ea3bba5a63181b329e3d0a90493c985223c6d0c
Sansevieria <elizabethleighmather@gmail.com>
2019-04-28 12:10:00 -0500

    added some styling and additional response data that was still needed

--- liri.js
+++ liri.js
@@ -34,7 +34,7 @@ switch (action) {
         findSong();
         break;
 
-    case "movie-this":
+    case "find-movie":
         findMovie();
         break;
 
@@ -51,11 +51,21 @@ function findConcert() {
             for (var i = 0; i < response.data.length; i++) {
                 // console.log(response.data[i].venue.name + ", " + response.data[i].venue.city + ", " + 
                 // response.data[i].venue.region + ", " + response.data[i].datetime);
-                console.log("=================================================================");
+
+                // moment js
+                var initialFormat = "";
+                var convert = moment(time, initialFormat);
+                var reformatTime = convert.format("LLLL");
+                var time = response.data[i].datetime;
+
+                // moment.js countdown
+                var fromNow = moment(reformatTime).endOf('day').fromNow();
+
+                console.log("======================== ♩ ♬ ♩ ♬ ========================");
                 console.log(response.data[i].venue.name);
                 console.log(response.data[i].venue.city + ", " + response.data[i].venue.region);
-                console.log(response.data[i].datetime);
-                console.log("=================================================================");
+                console.log(reformatTime + " (" + fromNow + ")");
+                console.log("======================== ♩ ♬ ♩ ♬ ========================");
             };
         });
 };
@@ -67,10 +77,14 @@ function findSong() {
     .then(function(response) {
         var songData = response.tracks.items;
 
-        console.log("=================================================================");
-        console.log(songData[0].artists);
-        // console.log(response.tracks.items[0].external_urls);
-        console.log("=================================================================");
+        console.log("======================== ♩ ♬ ♩ ♬ ========================");
+        // console.log(songData[0]);
+        console.log("Artist: " + songData[0].album.artists[0].name);
+        console.log("Song: " + songData[0].name);
+        console.log("From album: " + songData[0].album.name);
+        console.log("Tracks on album: " + songData[0].album.total_tracks);
+        console.log("Spotify URL: " + songData[0].album.external_urls.spotify);
+        console.log("======================== ♩ ♬ ♩ ♬ ========================");
         // console.log(response.tracks.items[0].album.artists);
         // console.log(response.tracks.items[0].artists);
         // console.log("line 69: " + response.tracks.items[0].artists.name);
@@ -89,9 +103,15 @@ function findMovie() {
             }
             else {
                 console.log("=================================================================");
-                console.log(response.data.Title);
-                console.log(response.data.Year);
+                // console.log(response.data);
+                console.log(response.data.Title + ", " + response.data.Year);
                 console.log(response.data.Plot);
+                console.log("\n  IMDB rating: " + response.data.Ratings[0].Value);
+                console.log("  Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+                console.log("  Awards/Nominations: " + response.data.Awards);
+                console.log("  Country: " + response.data.Country);
+                console.log("  Language: " + response.data.Language);
+                console.log("  Actors: " + response.data.Actors);
                 console.log("=================================================================");
             }
         });
